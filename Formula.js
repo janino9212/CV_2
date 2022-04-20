64ff4ce0354e2a4b8ff49ea306deaab5fa688d9b
SALA ESTUDIANTES 10 <aula10@salasuts.uts.edu.co>
2022-04-19 19:51:27 -0500

    hola

---
+++ Formula.js
@@ -0,0 +1,33 @@
+function ingresar() {
+  var cadena;
+  cadena = prompt("ingrese nombre");
+  return cadena;
+}
+function correo() {
+  var correo;
+  correo = prompt("ingrese correo electrónico");
+  return correo;
+}
+function telefono() {
+  var tel;
+  tel = prompt("ingrese número telefónico");
+  return tel;
+}
+
+function estudios() {
+  var estudio;
+  estudio = prompt("ingrese estudio realizado e institucion");
+  return estudio;
+}
+
+function periodo() {
+  var periodo;
+  periodo = prompt("ingrese periodo de estudio");
+  return periodo;
+}
+
+function perfil() {
+  var perfil;
+  perfil = prompt("ingrese su perfil profesional");
+  return perfil;
+}
